41aba5b58ae2e730e25f8ceefc908385a9a6c55a
Denis <iXtr3m@gmail.com>
2013-11-21 09:27:03 +0200

    Update main.cpp
    Вывод пользователю сообщения с условием уравнения.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
   SetConsoleOutputCP(1251)
   int a, b;
   float x;
+  cout << "Попробуем решить простое уравнение a/b:" << endl;
   cout << "Введите значение а" << endl;
   cin >> a;
   cout << "Введите значение b" << endl;
